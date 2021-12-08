a690364be14797ac4bf6670a124066977c6de2dd
zero <94368175+zerosquad13@users.noreply.github.com>
2021-12-08 12:49:44 +0700

    Update logomaker.py

--- EmikoRobot/modules/logomaker.py
+++ EmikoRobot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [〝ⵢɪɴɴᴇʀ〞](https://t.me/lifeinsinn)",
+            caption="Logo by [〝ⵢɪɴ͜͡ɴᴇʀ〞](https://t.me/lifeinsinn)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [〝ⵢɪɴɴᴇʀ〞](https://t.me/lifeinsinn)",
+            caption="Logo by [〝ⵢɪɴ͜͡ɴᴇʀ〞](https://t.me/lifeinsinn)",
             force_document=False,
         )
         os.remove(flnme)
